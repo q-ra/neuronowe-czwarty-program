d1ab17e45e207df0a1aba1846feb58f6d4babf5a
Michał Kurkowski <michalkurkowski93@gmail.com>
2016-12-31 18:20:46 +0100

    poprawki

--- actions.js
+++ actions.js
@@ -1,13 +1,13 @@
-var examples = [] //Przykłady
-var neurons = []
-var lastPos = { x: 0, y: 0 } //Ostatnia znana pozycja przykłądu
-var painting = $('.painting')[0]
-var ctx = painting.getContext('2d') //canvas
+let examples = [] //Przykłady
+let weights = []
+let lastPos = { x: 0, y: 0 } //Ostatnia znana pozycja przykłądu
+let painting = $('.painting')[0]
+let ctx = painting.getContext('2d') //canvas
 
 
-var offests = painting.getBoundingClientRect();
-var offset_left = offests.left
-var offset_top = offests.top
+let offests = painting.getBoundingClientRect();
+let offset_left = offests.left
+let offset_top = offests.top
 
 
 // Funkcja rysująca graf
@@ -39,16 +39,21 @@ function setPosition(e) {
   lastPos.y = e.clientY - offset_top;
 }
 
-function redrawExamples(){
+function redrawExamples() {
   ctx.lineWidth = 10;
   ctx.lineCap = 'round';
   ctx.strokeStyle = 'green';
   ctx.beginPath();
-  for(let example of examples){
+  for (let example of examples) {
     ctx.moveTo(example.x, example.y);
     ctx.lineTo(example.x, example.y);
   }
   ctx.stroke();
+}
+
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, 500, 500);
 }
 
 function draw(e) {
@@ -64,57 +69,66 @@ function draw(e) {
   ctx.stroke();
 }
 
-
-$('.js-rand').on('click', function () {
-  for (let it = 0; it < $('.js-number-of-points').val(); it += 1) {
-    neurons.push(new Point(parseInt(Math.random() * 500), parseInt(Math.random() * 500)))
-  }
-  drawPointsAndLines(neurons)
-})
-
-$('.js-reset').on('click', function(){
-  examples = [] 
-  neurons = []
-  lastPos = { x: 0, y: 0 }
-  clearCanvas()
-})
-
 function alpha(t, T) {
   return 1 - ((t - 1) / T);
 }
 
-function clearCanvas() {
-  ctx.clearRect(0, 0, 500, 500);
-}
+$('.js-rand').on('click', function () {
+  for (let it = 0; it < $('.js-number-of-points').val(); it += 1) {
+    weights.push(new Point(parseInt(Math.random() * 500), parseInt(Math.random() * 500)))
+  }
+  drawPointsAndLines(weights)
+})
+
+$('.js-number-of-points').on('change', function(){
+  $('.js-number-of-points-l').text($('.js-number-of-points').val())
+})
+
+$('.js-t').on('change', function(){
+  $('.js-t-l').text($('.js-t').val())
+})
+
+$('.js-reset').on('click', function () {
+  examples = []
+  weights = []
+  lastPos = { x: 0, y: 0 }
+  clearCanvas()
+})
+
+$('.js-debugger').on('click', function () {
+  debugger
+})
+
+
 
 $('.js-start').on('click', function () {
   T = $('.js-t').val()
   for (let t = 0; t <= T; t += 1) {
 
     currentExample = examples[Math.floor(Math.random() * examples.length)]
-    currentDistance =  neurons[0].distance(currentExample)
+    currentDistance = weights[0].distance(currentExample)
     // [itTmp, distanceTmp] = [0, 0]
     itTmp = 0
     distanceTmp = 0
-    for (let itNeuron of Object.keys(neurons)) {
+    for (let itWeight of Object.keys(weights)) {
       // debugger
 
-      distanceTmp = neurons[itNeuron].distance(currentExample)
+      distanceTmp = weights[itWeight].distance(currentExample)
       if (distanceTmp < currentDistance) {
-        [currentDistance, itTmp] = [distanceTmp, itNeuron]
+        [currentDistance, itTmp] = [distanceTmp, itWeight]
       }
-      for (i = 0; i < 2; i++) {
-        G = 1.0 / (i + 1);
-        if (itTmp + i < neurons.length) {
-          neurons[parseInt(itTmp) + i].update(alpha(t, T), G, currentExample);
+      for (let it of [0, 1]) {
+        G = 1.0 / (it + 1);
+        if (itTmp + it < weights.length) {
+          weights[parseInt(itTmp) + it].update(alpha(t, T), G, currentExample);
         }
-        if (parseInt(itTmp) - i >= 0) {
-          neurons[parseInt(itTmp) - i].update(alpha(t, T), G, currentExample);
+        if (parseInt(itTmp) - it >= 0) {
+          weights[parseInt(itTmp) - it].update(alpha(t, T), G, currentExample);
         }
       }
     }
   }
   clearCanvas()
-  drawPointsAndLines(neurons)
+  drawPointsAndLines(weights)
   redrawExamples()
 }) 
